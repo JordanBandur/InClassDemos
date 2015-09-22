2c527a6674f8233a630fd1c421894250b6f2a121
Jordan <jordanbandur@hotmail.ca>
2015-09-21 20:48:36 -0600

    Setting up the component library and initial entities

---
+++ eRestaurantDemo/eRestaurantSystem/DAL/Entities/Reservation.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Namespaces
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+#endregion
+
+
+namespace eRestaurantSystem.DAL.Entities
+{
+    public class Reservation
+    {
+        [Key]
+        public int ReservationID { get; set; }
+        public string CustomerName { get; set; }
+        public DateTime ReservationDate { get; set; } 
+        public int NumberInParty { get; set; }
+        public string ContactPhone { get; set; }
+        public string ReservationStatus { get; set; }
+        public string EventCode { get; set; }
+
+        //Navigation properties
+        public virtual SpecialEvent Event { get; set; }
+
+        //the Reservations table (sql) is a many to many relationship to the Tables table (sql)
+        //sql solves this proble by having an associate table that has a compound primary key
+        //created from Reservations and tables
+
+        //we will not be creating an entity for this associate table.
+        //instead we will create an overload map in our DbContext class however, we can still
+        //create the virtual navigation property to accodmodate this relationship
+        public virtual ICollection<Table> Tables { get; set; }
+    }
+}
---
+++ eRestaurantDemo/eRestaurantSystem/DAL/Entities/Table.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Namespaces
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+#endregion
+
+
+namespace eRestaurantSystem.DAL.Entities
+{
+    public class Table
+    {
+        [Key]
+        public int TableID { get; set; }
+        public byte TableNumber { get; set; }
+        public bool Smoking { get; set; }
+        public int Capacity { get; set; }
+        public bool Available { get; set; }
+
+        //Navigational properties
+        public virtual ICollection<Reservation> Reservations { get; set; }
+    }
+}
